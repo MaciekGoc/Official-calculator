7180d0205f8754764e6be14317d1baaef1aa067e
MaciekGoc <mggocu@gmail.com>
2022-02-03 20:02:59 +0100

    Add project files.

---
+++ Official calculator/Calculator/Calculator.cs
@@ -0,0 +1,56 @@
+﻿namespace Official_calculator
+{
+    class Calculator<T>
+    {
+        public Calculator()
+        {
+            var example = default(T);
+            var isInt = (example is int);
+            var isFloat = (example is float);
+            var isDouble = (example is double);
+
+            if (isInt)
+            {
+
+            }
+            else if (isFloat)
+            {
+
+            }
+            else if (isDouble)
+            {
+
+            }
+            else
+            {
+                System.Console.WriteLine("You have made a mistake. Start again.");
+            }
+        }
+
+        public T Add(T first, T second)
+        {
+            dynamic firstNumber = first;
+            dynamic secondNumber = second;
+            return firstNumber + secondNumber;
+        }
+        public T Subtract(T first, T second)
+        {
+            dynamic firstNumber = first;
+            dynamic secondNumber = second;
+            return firstNumber - secondNumber;
+        }
+        public T Multiply(T first, T second)
+        {
+            dynamic firstNumber = first;
+            dynamic secondNumber = second;
+            return firstNumber * secondNumber;
+        }
+        public T Divide(T first, T second)
+        {
+            dynamic firstNumber = first;
+            dynamic secondNumber = second;
+            return firstNumber / secondNumber;
+        }
+
+    }
+}
---
+++ Official calculator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Official_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator<double>();
+
+            Console.WriteLine("Welcome to my calculator!");
+
+            Console.WriteLine("Please put first number: ");
+            var firstNumber = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please put second number: ");
+            var secondNumber = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please put which operation would you like to make.");
+            Console.WriteLine("You can choose one of these: + - * /");
+
+            var operation = Console.ReadLine();
+
+            var result = default(double);
+
+            switch (operation)
+            {
+                case "+":
+                    {
+                        result = calculator.Add(firstNumber, secondNumber);
+                    } break;
+                case "-":
+                    {
+                        result = calculator.Subtract(firstNumber, secondNumber);
+                    } break;
+                case "*":
+                    {
+                        result = calculator.Multiply(firstNumber, secondNumber);
+                    } break;
+                case "/":
+                    if (secondNumber == 0)
+                    {
+                        Console.WriteLine("You can't divide by ZERO!");
+                    }
+                    else
+                    {
+                        result = calculator.Divide(firstNumber, secondNumber);
+                    } break;
+                default:
+                    {
+                        Console.WriteLine("Wrong action! Start again!");
+                    }
+                    break;
+
+
+            }
+            Console.WriteLine("Result of your operation is: " + result);
+            Console.WriteLine("Press any key to close the window.");
+        }
+    }
+}
